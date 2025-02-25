8687f0b3ab9b0f6c1c2cf79ac7770e6c886c9bd2
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2025-02-25 13:42:11 +0530

    more tests added

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -47,6 +47,7 @@ describe 'database' do
     expect(result).to match_array([
       "> Row inserted successfully.",
       "> [SELECT] (1 user1 user1@example.com)",
+      "Returned 1 rows.",
       "> Encountered exit, exiting..."
     ])
   end
@@ -62,6 +63,68 @@ describe 'database' do
     result = run_script(script)
     expect(result[-3]).to eq("> Row inserted successfully.")
     expect(result[-2]).to eq("> [ERROR] Table is full, cannot insert the row")
+  end
+
+  it "Allows string columns till they don't exceed max length" do
+    username = "a" * 32
+    email = "a" * 255
+    row = "1 #{username} #{email}"
+    insert = "insert #{row}"
+
+    script = [
+      insert,
+      "select",
+      ".exit"
+    ]
+
+    result = run_script(script)
+    result.shift()
+    
+    expect(result).to match_array([
+      "> Row inserted successfully.",
+      "> [SELECT] (#{row})",
+      "Returned 1 rows.",
+      "> Encountered exit, exiting..."
+    ])
+  end
+
+  it "Strings longer than allowed length not allowed, it should throw an error" do
+    username = "a" * 33
+    email = "a" * 256
+    insert = "insert 1 #{username} #{email}"
+
+    script = [
+      insert,
+      "select",
+      ".exit"
+    ]
+
+    result = run_script(script)
+    result.shift()
+    
+    expect(result).to match_array([
+      "> Token too long: #{insert}",
+      "> Returned 0 rows.",
+      "> Encountered exit, exiting..."
+    ])
+  end
+
+  it "Negative Id not allowed, it should throw an error" do
+    insert = "insert -1 user user@email.com"
+    script = [
+      insert,
+      "select",
+      ".exit"
+    ]
+
+    result = run_script(script)
+    result.shift()
+    
+    expect(result).to match_array([
+      "> Negative token found: #{insert}",
+      "> Returned 0 rows.",
+      "> Encountered exit, exiting..."
+    ])
   end
 end
   
